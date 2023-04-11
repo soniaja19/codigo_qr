a09204c15ee12fe893352cc8006eed530b30c781
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-04-11 13:08:33 -0500

    Mods

---
+++ lib/ui/general/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+//Se coloca la k en minúscula al inicio por ejemplo kBrandPrimary para definir un valor constante
+const Color kBrandPrimary = Color(0xff19F5BE);
+const Color kBrandSecundary = Color(0xff12151A);
