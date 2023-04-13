9e1bfdbbcf5191a8a216502b2131679a2ef4ba99
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-04-13 11:05:38 -0500

    Mods

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -32,11 +32,14 @@ class HistoryPage extends StatelessWidget {
                 ),
               ),
               ListView.builder(
-                shrinkWrap: true,
+                //physics: const ScrollPhysics(),
+                physics:
+                    const NeverScrollableScrollPhysics(), // para que se pueda movilizar la panatalla de arriba a bajo
+                shrinkWrap:
+                    true, //Delimita la cantidad de hijos que tiene el scroll para que no se rompa la pantalla
                 itemCount: 10,
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
-                    color: Colors.white.withOpacity(0.09),
                     margin: const EdgeInsets.symmetric(
                       vertical: 10.0,
                     ),
@@ -44,33 +47,71 @@ class HistoryPage extends StatelessWidget {
                       horizontal: 10.0,
                       vertical: 14.0,
                     ),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.09),
+                      borderRadius: BorderRadius.circular(14.0),
+                    ),
                     child: Row(
                       children: [
-                        Column(
-                          children: const [
-                            Text(
-                              "12/04/2023  20:31",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: const [
+                                  Icon(
+                                    Icons.calendar_month,
+                                    size: 14.0,
+                                    color: Colors.white54,
+                                  ),
+                                  Text(
+                                    " 12/04/2023  20:31",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                            Text(
-                              "Pasta Canteen",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16.0,
+                              const SizedBox(
+                                height: 3.0,
                               ),
-                            ),
-                            Text(
-                              "Carnes y parrillas",
-                              style: TextStyle(
-                                color: Colors.white54,
-                                fontSize: 14.0,
+                              const Text(
+                                "Pasta Canteen",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                ),
                               ),
-                            )
-                          ],
+                              const SizedBox(
+                                height: 3.0,
+                              ),
+                              const Text(
+                                "Carnes y parrillas",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  color: Colors.white54,
+                                  fontSize: 14.0,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.link,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.qr_code,
+                            color: Colors.white,
+                          ),
+                        )
                       ],
                     ),
                   );
